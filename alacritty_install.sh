fe901f836afc179814432ef2e889fabbe5eeeaf5
karlen <karlen.hayrapetyan75@gmail.com>
2024-01-31 21:57:02 +0400

    Some modifications

--- alacritty_install.sh
+++ alacritty_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S zsh alacritty neovim tmux
+sudo pacman -S alacritty neovim tmux
 
 # Create and set up Alacritty theme directory
 mkdir -p ~/.config/alacritty/themes
+
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
@@ -12,14 +14,10 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 git clone https://github.com/tom-auger/cmdtime ~/.oh-my-zsh/custom/plugins 
 source ~/.zshrc
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
---
+++ ohmyzsh_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -S zsh 
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
